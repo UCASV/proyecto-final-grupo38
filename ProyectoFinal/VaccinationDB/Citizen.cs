6c92a617dead5c22966960e2d466b41458bb2acb
Fergomez0901 <00147420@uca.edu.sv>
2021-06-17 16:44:58 -0600

    Uploading: Connection with Database

---
+++ ProyectoFinal/VaccinationDB/Citizen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ProyectoFinal.VaccinationDB
+{
+    public partial class Citizen
+    {
+        public Citizen()
+        {
+            Appointments = new HashSet<Appointment>();
+            CitizenxEmployees = new HashSet<CitizenxEmployee>();
+            Diseases = new HashSet<Disease>();
+            Vaccines = new HashSet<Vaccine>();
+        }
+
+        public string Dui { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Identifier { get; set; }
+
+        public virtual ICollection<Appointment> Appointments { get; set; }
+        public virtual ICollection<CitizenxEmployee> CitizenxEmployees { get; set; }
+        public virtual ICollection<Disease> Diseases { get; set; }
+        public virtual ICollection<Vaccine> Vaccines { get; set; }
+    }
+}
